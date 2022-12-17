07ba1f4fc32867eddc62571f45670760775be5d3
menthaGlacier <67158979+menthaGlacier@users.noreply.github.com>
2022-12-18 04:21:30 +0700

    Updated DavidKursach
    ya sdelal malo no eto chesnaya rabota

---
+++ KursachDavid/Menu.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "AdvertBoard.h"
+
+/* Класс меню для работы с доской объявлений */
+class Menu
+{
+private:
+	/* Доска объявлений и строка для ввода */
+	AdvertBoard board;
+	std::string inputLine;
+
+	/* Метод ввода нового элемента для вставок */
+	Advert newAdvert();
+	
+	/* Пункты меню*/
+	void insertAct();
+	void removeAct();
+	void saveAct();
+	void loadAct();
+	void sortAct();
+	void findAct();
+	void outputAct();
+public:
+	/* Конструктор по умолчанию */
+	Menu();
+};
